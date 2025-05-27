5b0a1e5cb2f1bea78e4ded1adeeefe845f8e641c
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-05-27 22:31:07 +0400

    Fix: Add configuration for Windows build

--- cmake/ConfigWindows.cmake
+++ cmake/ConfigWindows.cmake
@@ -1,2 +1,10 @@
 message(STATUS "Windows configuration loaded")
 set(OS_DEFINE OS_WINDOWS)
+
+foreach(flag_var
+    CMAKE_C_FLAGS_RELEASE CMAKE_C_FLAGS_DEBUG
+    CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_DEBUG)
+    string(REPLACE "/MT" "/MD" ${flag_var} "${${flag_var}}")
+endforeach()
+
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDLL")
