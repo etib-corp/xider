da5856fb04d107b5f4a5fd4c0a466ecdfe569138
Clément Antoine XAVER <clement-antoine.xavier@epitech.eu>
2026-03-26 00:03:21 +0900

    Feat: Add Android project

---
+++ platforms/android/app/src/main/cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <jni.h>
+
+#include <game-activity/native_app_glue/android_native_app_glue.h>
+#include <game-activity/GameActivity.h>
+
+extern "C" {
+
+void android_main(struct android_app *android_app) {}
+}
