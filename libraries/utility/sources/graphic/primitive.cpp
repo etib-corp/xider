f223d3c70afb753496a116ad308269263ad34d5e
Nathan Maillot <nathan.maillot@epitech.eu>
2026-04-28 16:44:23 +0900

    feat(utility): add base primitive

---
+++ libraries/utility/sources/graphic/primitive.cpp
@@ -0,0 +1,23 @@
+/*
+** ETIB PROJECT, 2026
+** xider
+** File description:
+** primitive
+*/
+
+#include <utility/graphic/primitive.hpp>
+
+namespace utility::graphic
+{
+    Primitive::Primitive(const std::vector<Mesh> &meshes)
+    {
+        for (const auto &mesh: meshes) {
+            addMesh(mesh);
+        }
+    }
+
+    void Primitive::addMesh(const Mesh &mesh)
+    {
+        _meshes.push_back(std::make_shared<Mesh>(mesh));
+    }
+}
