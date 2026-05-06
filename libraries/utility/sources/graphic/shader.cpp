17115d0b19aecb1eb089dc96bd048b7c0cd08028
Nathan Maillot <nathan.maillot@epitech.eu>
2026-05-06 13:10:51 +0900

    feat(utility): add shader code getters

--- libraries/utility/sources/graphic/shader.cpp
+++ libraries/utility/sources/graphic/shader.cpp
@@ -17,4 +17,18 @@ namespace utility::graphic
         _fragSPIRV = std::vector<uint32_t>(fragString.size() / sizeof(uint32_t));
         std::memcpy(_fragSPIRV.data(), fragString.data(), fragString.size());
     }
+
+    /////////////
+    // Getters //
+    /////////////
+
+    const std::vector<uint32_t> &Shader::getVertexCode() const
+    {
+        return _vertSPIRV;
+    }
+
+    const std::vector<uint32_t> &Shader::getFragmentCode() const
+    {
+        return _fragSPIRV;
+    }
 }
