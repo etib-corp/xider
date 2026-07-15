ae21af63300cca9400f04033ff2d9eb596775950
Nathan Maillot <nathan.maillot@epitech.eu>
2026-07-15 13:03:56 +0200

    fix(utility): trying texture mapping in ressource provider

--- libraries/utility/sources/ressource_provider.cpp
+++ libraries/utility/sources/ressource_provider.cpp
@@ -314,7 +314,7 @@ namespace utility
 		RessourceProvider::loadMaterial(const std::string &path,
 										ShaderType shaderType)
 	{
-		auto it = _elementsIDs.find(path);
+		auto it = _elementsIDs.find(resolvePath(path));
 
 		if (it != _elementsIDs.end()) {
 			if (_materials.find(it->second) != _materials.end()) {
@@ -457,14 +457,19 @@ namespace utility
 			}
 		}
 
-		auto materialID = getMaterialID(material);	// Default material ID if not found
+		auto materialID = getMaterialID(resolvePath(material));	// Default material ID if not found
 
 		if (materialID == 0) {
-			getLogger().warning()
-				<< "Material not found: " << material
-				<< ". Using default material for model: "
-				<< modelAsset->path();
-			materialID = getDefaultMaterialID();
+			loadMaterial(material, ShaderType::MESH_SHADER);
+			materialID = getMaterialID(resolvePath(material));
+
+			if (materialID == 0) {
+				getLogger().warning()
+					<< "Material not found: " << material
+					<< ". Using default material for model: "
+					<< modelAsset->path();
+				materialID = getDefaultMaterialID();
+			}
 		}
 
 		auto model = std::make_shared<graphic::Model>(modelAsset, materialID);
@@ -488,7 +493,7 @@ namespace utility
 			}
 		}
 
-		auto materialID = getMaterialID(material);
+		auto materialID = getMaterialID(resolvePath(material));
 
 		if (materialID == 0) {
 			getLogger().warning()
@@ -525,7 +530,7 @@ namespace utility
 			return nullptr;
 		}
 
-		auto materialID = getMaterialID(material);
+		auto materialID = getMaterialID(resolvePath(material));
 
 		if (materialID == 0) {
 			getLogger().warning()
@@ -555,7 +560,7 @@ namespace utility
 			}
 		}
 
-		auto materialID = getMaterialID(material);
+		auto materialID = getMaterialID(resolvePath(material));
 
 		if (materialID == 0) {
 			getLogger().warning()
